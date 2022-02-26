d956123c05eba395d54fb9887cf5e5ec07a60761
Lauresmi <esmite.laura@gmail.com>
2022-02-26 18:07:35 +0200

    test for Categories

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Content from "./Components/Content";
 
-
 function App() {
   return (
     <div className="App">
