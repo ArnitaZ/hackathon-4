d30eda7f9adc436ac2e781a79ad56c55bc9ff571
Lauresmi <esmite.laura@gmail.com>
2022-02-26 13:25:06 +0200

    Updated responsiveness

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,13 +2,13 @@ function Footer() {
 
     return (
         <div className="fixed-bottom">
-            <footer class="d-flex flex-wrap justify-content-between bg-light align-items-center py-3 my-4 border-top">
+            <footer class="d-flex flex-wrap justify-content-between bg-light align-items-center py-1 my-2 border-top">
 
                 <div class="col-md-4 d-flex align-items-center mx-4">
                     <span class="text-muted">&copy; 2022 Travel with us, Inc</span>
                 </div>
 
-                <ul class="nav col-md-4 justify-content-end list-unstyled d-flex mx-4">
+                <ul class="nav col-md-4 justify-content-end list-unstyled d-flex mx-4 mt-1">
 
                     <li class="ms-3">
                         <a class="text-muted"
