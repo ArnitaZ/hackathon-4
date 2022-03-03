8ad82e290b95156b1d09457ee36882b96b5cb65b
Arnita <arnitucis@inbox.lv>
2022-03-03 23:10:26 +0200

    Pagination
    Added to products page

---
+++ src/Components/Pagination.js
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+
+function Pagination() {
+  return (
+    <div>
+      <nav aria-label="Page navigation example">
+        <ul className="pagination">
+          <li className="page-item">
+            <Link className="page-link" to={""} aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link" to={""}>
+              1
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link" to={""}>
+              2
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link" to={""}>
+              3
+            </Link>
+          </li>
+          <li className="page-item">
+            <Link className="page-link" to={""} aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+export default Pagination;
